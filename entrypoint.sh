556ee6697a98ce6b053c73c144f5dd50acadbcc0
Pao Yu <51064667+pyxn@users.noreply.github.com>
2021-07-11 18:56:54 -0400

    v2.0.0: add mirror_file_create mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,64 @@
 #!/bin/sh
 
+# ---------------------------------------------------------------------------------------------------------------------
+# FUNCTIONS
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Mirror entire contents of local directory to remote directory
+function sftp_mirror_full() {
+  echo "Initializing LFTP file transfer: Full Mirror Mode..."
+  lftp ${PROTOCOL}://${HOSTNAME}:${PORT} -u ${USERNAME},${PASSWORD} -e "mirror ${ARGS} -R ${PATH_LOCAL} ${PATH_REMOTE}"
+  echo "SFTP Mirror operation completed. Please verify the status of your files."
+}
+
+# Mirror a single file created in local directory to remote directory
+function sftp_mirror_file_create() {
+
+  # Create a new file with specified filename and extension
+  touch "${CREATE_FILE_NAME}"
+  echo "Successfully created new file with filename ${CREATE_FILE_NAME}."
+
+  # Place specified contents into the new file
+  echo "${CREATE_FILE_CONTENTS}" >${CREATE_FILE_NAME}
+  echo "Successfully filled ${CREATE_FILE_NAME} with specified file contents."
+
+  # Start LFTP Transfer of single file
+  # main command option: put
+  # -e          delete target file before the transfer
+  # -E          delete source files after successful transfer (dangerous)
+  # -a          use ascii mode (binary is the default)
+  echo "Initializing LFTP file transfer: Single File Create Mirror Mode..."
+  lftp ${PROTOCOL}://${HOSTNAME}:${PORT} -u ${USERNAME},${PASSWORD} -e "put -E -e -a ${CREATE_FILE_NAME} -o ${PATH_REMOTE}${CREATE_FILE_NAME}"
+  echo "Successfully transferred ${CREATE_FILE_NAME} to specified directory."
+  echo "SFTP Mirror operation completed. Please verify the status of your files."
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# MAIN SCRIPT
+# ---------------------------------------------------------------------------------------------------------------------
 set -eu
 echo "SFTP Mirror"
 
-SELECTED_PROTOCOL=${PROTOCOL:-"ftp"}
-SELECTED_ARGS=${ARGS:-""}
-
-SELECTED_PATH_LOCAL=${PATH_LOCAL:-"."}
-SELECTED_PATH_REMOTE=${PATH_REMOTE:-"."}
-
-if [ $SELECTED_PROTOCOL = "sftp" ]
-then
-  SELECTED_PORT=${PORT:-"22"}
+if [ ${PROTOCOL} = "sftp" ]; then
   echo "Establishing SFTP connection..."
-  sshpass -p $PASSWORD sftp -o StrictHOSTKeyChecking=no -P $SELECTED_PORT $USERNAME@$HOSTNAME
-  echo "Connecting to PORT $SELECTED_PORT via SFTP..."
+  sshpass -p ${PASSWORD} sftp -o StrictHOSTKeyChecking=no -P ${PORT} ${USERNAME}@${HOSTNAME}
+  echo "Connecting to PORT ${PORT} via SFTP..."
 else
   echo "Establishing FTP connection..."
-  SELECTED_PORT=${PORT:-"21"}
-  echo "Connecting to PORT $SELECTED_PORT via FTP..."
-fi;
+  PORT=${PORT:-"21"}
+  echo "Connecting to PORT ${PORT} via FTP..."
+fi
 
-echo "Initializing LFTP file transfer..."
-lftp $SELECTED_PROTOCOL://$HOSTNAME:$SELECTED_PORT -u $USERNAME,$PASSWORD -e "mirror $SELECTED_ARGS -R $SELECTED_PATH_LOCAL $SELECTED_PATH_REMOTE; quit"
+case ${MODE} in
+mirror_full)
+  sftp_mirror_full
+  ;;
+mirror_file_create)
+  sftp_mirror_file_create
+  ;;
+*)
+  sftp_mirror_full
+  ;;
+esac
 
-echo "SFTP Mirror operation completed. Please verify the status of your files."
 exit 0
